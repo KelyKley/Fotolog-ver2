4aa4eedb920b6c85869fbf3992016d490bf39cb3
kelyanamuro <kelyanamurohuamani@gmail.com>
2017-09-08 18:00:24 -0500

    Arreglo version 2

--- js/main.js
+++ js/main.js
@@ -1,43 +1,44 @@
-function guardar() {
-    var nombre = document.getElementById("nombre").value;
-    var comentario = document.getElementById("comentario").value;
-    localStorage.setItem(nombre, comentario);
-
-    var contenedor = document.getElementById("comentarios");
-    contenedor.innerHTML = "";
-
-    for (var i = 0; i < localStorage.length; i++) {
-        var nombre = localStorage.key(i);
-        var comentario = localStorage.getItem(nombre);
-
-        var div = document.createElement("DIV");
-        div.setAttribute("class", "div");
-
-        var cont_nombre = document.createElement("h3");
-        var texto = document.createTextNode(nombre);
-        cont_nombre.appendChild(texto);
-
-        var cont_comentario = document.createElement("P");
-        var texto_comentario = document.createTextNode(comentario);
-        cont_comentario.appendChild(texto_comentario);
-
-        div.appendChild(cont_nombre);
-        div.appendChild(cont_comentario);
-
-        contenedor.appendChild(div);
-
-        document.getElementById("nombre").value = "";
-        document.getElementById("comentario").value = "";
+const app = {
+    item: {
+        nombre: undefined,
+        comentario: undefined
+    },
+    init: function() {
+        app.item.nombre = $("#nombre");
+        app.item.comentario = $("#comentario");
+        app.todo();
+        app.configuracionDeBtn();
+    },
+    configuracionDeBtn: function() {
+        $("#guardar").click(app.guardar);
+        $("#limpiar").click(app.limpiar_comentario);
+        $("#eliminar").click(app.eliminar);
+    },
+    guardar: function() {
+        localStorage.setItem(app.item.nombre.val(), app.item.comentario.val());
+        $("#comentarios").append(`<div><h3>${app.item.nombre.val()}</h3>\
+            <p>${app.item.comentario.val()}</p></div>`);
+        app.limpiar();
+    },
+    limpiar: function() {
+        $("#nombre").val("").focus();
+        $("#comentario").val("");
+    },
+    limpiar_comentario: function() {
+        $("#comentarios").remove();
+    },
+    todo: function() {
+        for (let i = 0; i < localStorage.length; i++) {
+            let nombre = localStorage.key(i);
+            let comentario = localStorage.getItem(nombre);
+            $("#comentarios").append(`<div><h3>${nombre}</h3>\
+            <p>${comentario}</p></div>`);
+        }
+    },
+    eliminar: function() {
+        localStorage.clear();
+        app.limpiar_comentario();
     }
 }
 
-guardar();
-
-function limpiar() {
-    document.getElementById("comentarios").innerHTML = "";
-}
-
-function eliminar() {
-    localStorage.clear();
-    guardar();
-}
+$(document).ready(app.init);
